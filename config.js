bf67576d99fdd26886dc16a7aabdca09b174e1c6
Bradley Reed <shenmusha@yahoo.com>
2021-09-14 20:35:51 -0400

    facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '545775993170954',
+        clientSecret: '3689d0ba0cf8fd1a92e8be486efc6908'
+    }
 }
